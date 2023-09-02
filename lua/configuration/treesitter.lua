f29f387a4373eef711301addc4b95381bc6d7f36
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-09-02 21:34:20 +0200

    plugin: treesitter configuration

---
+++ lua/configuration/treesitter.lua
@@ -0,0 +1,48 @@
+--- treesitter.lua
+-- Configuration for nvim-treesitter plugin.
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+    'c',
+    'cpp',
+    'cmake',
+    'lua',
+    'bash',
+    'bibtex',
+    'css',
+    'diff',
+    'comment',
+    'dockerfile',
+    'git_config',
+    'git_rebase',
+    'groovy',
+    'html',
+    'json',
+    'xml',
+    'yaml',
+    'java',
+    'kotlin',
+    'latex',
+    'lua',
+    'luadoc',
+    'luap',
+    'markdown_inline',
+    'ninja',
+    'python',
+    'rust',
+    'ruby',
+    'sql',
+    'toml',
+    'verilog',
+    'vim',
+    'vimdoc'
+  },
+
+  sync_install = false,
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
