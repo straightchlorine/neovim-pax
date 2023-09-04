5b624ab292f8604aa3a17aa9502b405d0bad1b60
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-09-04 19:15:39 +0200

    plugins: added plugins required for autocompletion

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -1,3 +1,8 @@
+--- packer.lua
+-- All plugins for packer to install along
+-- with their configurations.
+---
+
 local config = require('configuration')
 
 return require('packer').startup(function (use)
@@ -9,4 +14,15 @@ return require('packer').startup(function (use)
     run = ':TSUpdate',
     config = config.treesitter,
   }
+
+  -- autocompletion plugins
+  use { 'neovim/nvim-lspconfig' }
+  use { 'hrsh7th/cmp-nvim-lsp' }
+  use { 'hrsh7th/cmp-buffer' }
+  use { 'hrsh7th/cmp-path' }
+  use { 'hrsh7th/cmp-cmdline' }
+  use { 'hrsh7th/ncim-cmp' }
+  use { 'SirVer/ultiships' }
+  use { 'quangnguyen30192/cmp-nvim-ultisnips' }
+
 end)
