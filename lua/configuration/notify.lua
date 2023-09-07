17c9d93be6bd87980bbf5bda16d0d990d6dec347
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-09-07 19:52:58 +0200

    plugin: notification plugin configuration

---
+++ lua/configuration/notify.lua
@@ -0,0 +1,25 @@
+--- notify.lua
+-- Configuration for nvim-notify plugin
+---
+
+local notification = require('notify')
+
+notification.setup {
+  background_colour = 'NotifyBackground',
+  fps = 165,
+  icons = {
+    DEBUG = '',
+    ERROR = '',
+    INFO = '',
+    TRACE = '✎',
+    WARN = ''
+  },
+  level = 2,
+  minimum_width = 50,
+  render = 'default',
+  stages = 'fade_in_slide_out',
+  timeout = 5000,
+  top_down = true
+}
+
+vim.notify = notification
