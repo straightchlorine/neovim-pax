248939e3af10733a0ecaf868f8608f6cb568cb92
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-09-07 20:12:37 +0200

    module: updated references;
    Added cmp-nvim-ultisnips, nvim-notify and nvim-tree
    configuration files as references in init.lua.

--- lua/configuration/init.lua
+++ lua/configuration/init.lua
@@ -1,7 +1,23 @@
-local configuration = {}
+--- init.lua
+-- Initialization of the configuration module
+-- containing configs for all the plugins.
+---
 
+local configuration = {}
   configuration.treesitter = function ()
     require('configuration.treesitter')
+  end
+
+  configuration.cmp_ultisnips = function ()
+    require('configuration.cmp_ultisnips')
+  end
+
+  configuration.notify = function ()
+    require('configuration.notify')
+  end
+
+  configuration.tree = function ()
+    require('configuration.tree')
   end
 
 return configuration
