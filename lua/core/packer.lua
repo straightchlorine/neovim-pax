af05c5c75ff446ee78e8cc4bce94c822ee9bac0b
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-09-09 22:16:05 +0200

    plugins: autocompletion plugins

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -12,17 +12,39 @@ return require('packer').startup(function (use)
 
   use { 'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate',
-    config = config.treesitter,
+    config = config.treesitter
   }
 
   -- autocompletion plugins
-  use { 'neovim/nvim-lspconfig' }
-  use { 'hrsh7th/cmp-nvim-lsp' }
+  use { 'neovim/nvim-lspconfig',
+    requires = { 'hrsh7th/cmp-nvim-lsp' },
+    config = config.lspconfig
+  }
+
   use { 'hrsh7th/cmp-buffer' }
   use { 'hrsh7th/cmp-path' }
   use { 'hrsh7th/cmp-cmdline' }
-  use { 'hrsh7th/ncim-cmp' }
+  use { 'petertriho/cmp-git' }
+
+  use { 'hrsh7th/nvim-cmp',
+    config = config.cmp
+  }
+
   use { 'SirVer/ultiships' }
-  use { 'quangnguyen30192/cmp-nvim-ultisnips' }
+
+  use { 'quangnguyen30192/cmp-nvim-ultisnips',
+    config = config.cmp_ultisnips
+  }
+
+  use { 'rcarriga/nvim-notify',
+    config = config.notify
+  }
+
+  use { 'nvim-tree/nvim-tree.lua',
+    requires = 'nvim-tree/nvim-web-devicons',
+    config = config.tree,
+  }
+
+  use { 'nvim-lualine/lualine.nvim' }
 
 end)
