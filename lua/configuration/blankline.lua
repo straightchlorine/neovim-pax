c0d72a6446d9c7495048c6325d7a9bb1249f0eba
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-09-11 19:49:14 +0200

    plugin: indent_blankline.nvim configuration

---
+++ lua/configuration/blankline.lua
@@ -0,0 +1,9 @@
+--- blankline.lua
+-- Configuration for indent-blankline.nvim plugin
+---
+
+require('indent_blankline').setup {
+    space_char_blankline = ' ',
+    show_current_context = true,
+    show_current_context_start = true
+}
