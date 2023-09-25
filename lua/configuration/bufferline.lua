fa2f5bc5434246b0abdb548910eaba8b489ce632
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-09-25 19:58:07 +0200

    plugin: updated configuration for bufferline plugin

--- lua/configuration/bufferline.lua
+++ lua/configuration/bufferline.lua
@@ -2,9 +2,12 @@
 -- Configuration for bufferline.nvim plugin
 ---
 
-require('bufferline').setup {
+local bufferline = require('bufferline')
+
+bufferline.setup {
   options = {
-    mode = "buffers",
+    mode = 'buffers',
+    style_preset = bufferline.style_preset.minimal,
     themable = true,
     numbers = 'both',
     close_command = "bdelete! %d",
@@ -15,7 +18,7 @@ require('bufferline').setup {
       icon = '▎',
       style = 'icon',
     },
-    buffer_close_icon = '',
+    buffer_close_icon = '󰅖',
     modified_icon = '●',
     close_icon = '',
     left_trunc_marker = '',
@@ -29,40 +32,34 @@ require('bufferline').setup {
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
         return "("..count..")"
     end,
-    custom_filter = function(buf_number, buf_numbers)
-      -- excluded buffers
-    end,
     offsets = {
       {
         filetype = "NvimTree",
-        text = function()
-          return vim.fn.getcwd()
-        end,
-        text_align = "left",
-        separator = true
+        text = "File Tree" ,
+        text_align = "center",
+        separator = true,
       }
     },
     color_icons = true,
     get_element_icon = function(element)
-      local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(opts.filetype, { default = false })
+      local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(element.filetype, { default = false })
       return icon, hl
     end,
     show_buffer_icons = true,
     show_buffer_close_icons = false,
     show_close_icon = false,
     show_tab_indicators = true,
-    show_duplicate_prefix = true,
+    show_duplicate_prefix = false,
     persist_buffer_sort = true,
+    move_wraps_at_ends = false,
     separator_style = "thin",
-    enforce_regular_tabs = true,
+    enforce_regular_tabs =  true,
     always_show_bufferline = true,
     hover = {
       enabled = true,
       delay = 200,
       reveal = {'close'}
     },
-    sort_by = function(buffer_a, buffer_b)
-      return buffer_a.modified > buffer_b.modified
-    end
+    sort_by = 'relative_directory'
   }
 }
