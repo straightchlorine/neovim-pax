5f275ce9d97834911ea96e5a9a66508e8653ada5
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-09-25 20:58:39 +0200

    plugin: added separate file in for setting the theme

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,7 +1,12 @@
+--- core/init.lua
+-- Initializing core as a module.
+---
+
 local load_core = function ()
   require('core.packer')
   require('core.options')
   require('core.mappings')
+  require('core.theme')
 end
 
 load_core()
---
+++ lua/core/theme.lua
@@ -0,0 +1,7 @@
+--- theme.lua
+-- Setting the theme.
+---
+
+require('boo-colorscheme').use({
+  theme = 'forest_stream'
+})
