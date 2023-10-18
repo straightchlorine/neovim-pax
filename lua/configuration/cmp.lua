e97c782037a40492315dea10ff5a4239bafc0922
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-10-18 19:42:33 +0200

    cmp: added cmp_nvim_r as a source for the autocompletion engine

--- lua/configuration/cmp.lua
+++ lua/configuration/cmp.lua
@@ -129,6 +129,7 @@ cmp.setup({
     { name = 'nvim_lsp' },
     { name = 'nvim_lsp_signature_help' },
     { name = 'ultisnips' },
+    { name = 'cmp_nvim_r' },
   }, {
     { name = 'buffer' },
     { name = 'path' },
