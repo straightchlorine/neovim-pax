3c4f53cd31a7b00bc010cb1290289f5aa0e245ad
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-10-31 16:22:30 +0100

    packer: added markdown preview

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -214,4 +214,9 @@ return require('packer').startup(function (use)
   use { 'akinsho/flutter-tools.nvim',
     config = config.flutter_tools
   }
+
+  use { "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+    setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, 
+  }
 end)
