e05052e94f887042c5618a2ab157b067bf22a7d3
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-11-02 21:41:33 +0100

    hlslens: mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -9,4 +9,19 @@ vim.cmd('source $HOME/.config/nvim/lua/core/mappings/plugin.vim')
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
+-- nvim-hlslens
+local kopts = {noremap = true, silent = true}
+
+vim.api.nvim_set_keymap('n', 'n',
+  [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+  kopts
+)
+
+vim.api.nvim_set_keymap('n', 'N',
+  [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+  kopts
+)
+
+vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>noh<CR>', kopts)
+
 require('core.handles')
