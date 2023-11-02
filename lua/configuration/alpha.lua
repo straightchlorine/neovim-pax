9e3ca5a82ed3fff6b44954b60d40f01554e261ee
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-11-02 21:38:06 +0100

    alpha: formatting

--- lua/configuration/alpha.lua
+++ lua/configuration/alpha.lua
@@ -1,5 +1,7 @@
 --- alpha.lua
--- Configuration for alpha-nvim plugin.
+-- Configuration for alpha-nvim great.
 ---
 
-require('alpha').setup(require'alpha.themes.dashboard'.config)
+require('alpha').setup(require 'alpha.themes.dashboard'.config)
+
+-- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
