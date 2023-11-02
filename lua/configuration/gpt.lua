9d26ebaae7c6752fd6dcd1a469ed0d24fcd3557b
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-11-02 21:13:05 +0100

    chatgpt: snippet for formatting

--- lua/configuration/gpt.lua
+++ lua/configuration/gpt.lua
@@ -1,7 +1,9 @@
 --- gpt.lua
--- Configuration for ChatGPT.nvim plugin
+-- Configuration for ChatGPT.nvim plugin.
 ---
 
-require("chatgpt").setup {
+require('chatgpt').setup {
   api_key_cmd = os.getenv('OPENAI_API_KEY')
 }
+
+-- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
