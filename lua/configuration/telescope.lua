cf57a8f2357cd85ffab3eebd9024f42899181887
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-11-02 15:12:43 +0100

    telescope: added notification and dap modules

--- lua/configuration/telescope.lua
+++ lua/configuration/telescope.lua
@@ -6,10 +6,13 @@ require('telescope').setup{
   defaults = {
     mappings = {
       i = {
-        ["<C-h>"] = "which_key"
+        ['<C-h>'] = 'which_key'
       }
     }
   },
-  pickers = { },
-  extensions = { }
 }
+
+require('telescope').load_extension('notify')
+require('telescope').extensions.dap.configurations()
+
+-- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
