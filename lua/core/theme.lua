2bc266756102963d71fadde99f36bf41f07ff33a
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-11-02 22:45:48 +0100

    theme: formatting

--- lua/core/theme.lua
+++ lua/core/theme.lua
@@ -2,6 +2,8 @@
 -- Setting the theme.
 ---
 
-require('boo-colorscheme').use({
-  theme = 'forest_stream'
-})
+require('boo-colorscheme').use {
+  theme = 'sorbet'
+}
+
+-- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
