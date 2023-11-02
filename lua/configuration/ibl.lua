e2bb150cd17cc5ff78505ff41d61169cb5071675
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-11-02 14:07:41 +0100

    ibl: adding snippet for vim formatting

--- lua/configuration/ibl.lua
+++ lua/configuration/ibl.lua
@@ -6,6 +6,8 @@ require ('ibl').setup{
   debounce = 100,
   indent = { char = '┊' },
   whitespace = {
-    highlight = { "Whitespace", "NonText" },
+    highlight = { 'Whitespace', 'NonText' },
   },
 }
+
+-- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
