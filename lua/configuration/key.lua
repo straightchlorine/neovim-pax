8c6b810aaaa751301f4a57414dd5fab475fb25b0
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-11-02 22:02:46 +0100

    key: formatting

--- lua/configuration/key.lua
+++ lua/configuration/key.lua
@@ -2,4 +2,6 @@
 -- Configuration for which-key.nvim plugin
 ---
 
-require('which-key').setup { }
+require('which-key').setup {}
+
+-- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
