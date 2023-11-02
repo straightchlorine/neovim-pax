33909189cdec70d3d18e1938af7965ab933877e9
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-11-02 21:50:45 +0100

    colorizer: formatting

--- lua/configuration/colorizer.lua
+++ lua/configuration/colorizer.lua
@@ -2,4 +2,6 @@
 -- Configuraiton for nvim-colorizer.lua plugin
 ---
 
-require('colorizer').setup { }
+require('colorizer').setup {}
+
+-- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
