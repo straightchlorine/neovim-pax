05a5a3d05aeeaa3c3faa2d674289fe4db4a97de4
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-11-02 14:20:04 +0100

    flutter-tools: renamed the file and udpdated configuration module entry

---
+++ lua/configuration/flutter-tools.lua
@@ -0,0 +1,7 @@
+--- flutter-tools.lua
+-- Configuration for flutter-tools.nvim plugin.
+---
+
+require('flutter-tools').setup{}
+
+-- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=4:softtabstop=2:textwidth=80
