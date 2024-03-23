b129b1eb2b728c7ed88ef324a4a2326da4e253f9
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-03-23 18:52:23 +0100

    plugins: added vimtex settings in options module

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -153,3 +153,21 @@ vim.cmd([[
       \ ],
       \}
 ]])
+
+vim.cmd([[
+    let g:vimtex_syntax_conceal = {
+          \ 'accents': 1,
+          \ 'ligatures': 1,
+          \ 'cites': 1,
+          \ 'fancy': 1,
+          \ 'spacing': 1,
+          \ 'greek': 1,
+          \ 'math_bounds': 1,
+          \ 'math_delimiters': 1,
+          \ 'math_fracs': 1,
+          \ 'math_super_sub': 1,
+          \ 'math_symbols': 1,
+          \ 'sections': 0,
+          \ 'styles': 1,
+          \}
+]])
