42ac74e72fabaee2bb593a88b6af97e6b9bb5858
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-07-03 21:23:32 +0200

    refactor(barbar): replaced bufferline with barbar
    Never used the capabilities of bufferline to the fullest, will try
    something simpler.
    
    Bindings will not be moved to the config, setup is called automatically,
    no need to complicate matters.

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,14 @@
+return {
+	"romgrk/barbar.nvim",
+	dependencies = {
+		"lewis6991/gitsigns.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	init = function()
+		vim.g.barbar_auto_setup = false
+	end,
+	opts = {
+		exclude_ft = { "qf", "gitcommit" },
+	},
+	version = "^1.0.0",
+}
