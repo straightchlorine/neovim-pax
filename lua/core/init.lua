3d25faecb18684452a65ab0b3e06e92f2e57fbc5
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-07-04 13:07:57 +0200

    refactor(core): main config
    Loads lazy, options as well as mappings in such order.

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,12 +1,11 @@
 --- core/init.lua
--- Initializing core as a module.
+-- Initialize core as a module
 ---
 
-local load_core = function ()
-  require('core.packer')
-  require('core.options')
-  require('core.mappings')
-  require('core.theme')
+local load_core = function()
+	require("core.lazy")
+	require("core.options")
+	require("core.mappings")
 end
 
 load_core()
