fdc0ce72bf7af7b7727d727e4c266c061cab4919
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-07-08 14:58:46 +0200

    refactor(colorschemes): single file with all the colorschemes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	dependencies = {
+		"norcalli/nvim-base16.lua",
+		"navarasu/onedark.nvim",
+		"jacoborus/tender.vim",
+		"bluz71/vim-moonfly-colors",
+		"bluz71/vim-moonfly-colors",
+		"savq/melange-nvim",
+		"ellisonleao/gruvbox.nvim",
+		"kepano/flexoki",
+		"mcchrish/vim-no-color-collections",
+		"mcchrish/zenbones.nvim",
+		"nordtheme/vim",
+		"cocopon/iceberg.vim",
+	},
+	config = function()
+		vim.cmd([[colorscheme tokyonight]])
+	end,
+}
