fbec9b10526bd1df7761e06b72c64c419793ff56
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-07-08 14:55:58 +0200

    refactor(arduino): plugin for arduino support via cli
    Up for testing

---
+++ lua/plugins/vim-arduino.lua
@@ -0,0 +1,16 @@
+return {
+	"stevearc/vim-arduino",
+	config = function()
+		local keymap = vim.keymap
+
+		keymap.set("n", "<leader>aa", "<cmd>ArduinoAttach<CR>", { desc = "arduino: attach" })
+		keymap.set("n", "<leader>av", "<cmd>ArduinoVerify<CR>", { desc = "arduino: verify" })
+		keymap.set("n", "<leader>au", "<cmd>ArduinoUpload<CR>", { desc = "arduino: upload" })
+		keymap.set("n", "<leader>aus", "<cmd>ArduinoUploadAndSerial<CR>", { desc = "arduino: upload and serial" })
+		keymap.set("n", "<leader>as", "<cmd>ArduinoSerial<CR>", { desc = "arduino: serial" })
+		keymap.set("n", "<leader>ab", "<cmd>ArduinoChooseBoard<CR>", { desc = "arduino: board" })
+		keymap.set("n", "<leader>ap", "<cmd>ArduinoChooseProgrammer<CR>", { desc = "arduino: programmer" })
+	end,
+}
+
+-- TODO: test this plugin
