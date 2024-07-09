403a5474fd82e2a274cc68f1346903dcd2be4c09
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-07-09 18:54:47 +0200

    refactor(gitsigns): changed descritptions for easy lookup

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -9,18 +9,18 @@ return {
 				vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
 			end
 
-			map("n", "]h", gs.next_hunk, "next hunk")
-			map("n", "[h", gs.prev_hunk, "prev hunk")
+			map("n", "]h", gs.next_hunk, "gitsigns: next hunk")
+			map("n", "[h", gs.prev_hunk, "gitsigns: prev hunk")
 
-			map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "select hunk")
+			map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "gitsigns: select hunk")
 
-			map("n", "<leader>hs", gs.stage_hunk, "stage hunk")
-			map("n", "<leader>hp", gs.preview_hunk, "preview hunk")
-			map("n", "<leader>hu", gs.undo_stage_hunk, "undo stage hunk")
+			map("n", "<leader>hs", gs.stage_hunk, "gitsigns: stage hunk")
+			map("n", "<leader>hp", gs.preview_hunk, "gitsigns: preview hunk")
+			map("n", "<leader>hu", gs.undo_stage_hunk, "gitsigns: undo stage hunk")
 
-			map("n", "<leader>hS", gs.stage_buffer, "stage buffer")
-			map("n", "<leader>hR", gs.reset_buffer, "reset buffer")
-			map("n", "<leader>hr", gs.reset_hunk, "reset hunk")
+			map("n", "<leader>hS", gs.stage_buffer, "gitsigns: stage buffer")
+			map("n", "<leader>hR", gs.reset_buffer, "gitsigns: reset buffer")
+			map("n", "<leader>hr", gs.reset_hunk, "gitsigns: reset hunk")
 
 			map("v", "<leader>hs", function()
 				gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
@@ -30,17 +30,17 @@ return {
 				gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
 			end, "reset hunk")
 
-			map("n", "<leader>hB", gs.toggle_current_line_blame, "toggle blame")
+			map("n", "<leader>hB", gs.toggle_current_line_blame, "gitsigns: toggle blame")
 
 			map("n", "<leader>hb", function()
 				gs.blame_line({ full = true })
-			end, "blame")
+			end, "gitsigns: blame")
 
-			map("n", "<leader>hd", gs.diffthis, "diff")
+			map("n", "<leader>hd", gs.diffthis, "gitsigns: diff")
 
 			map("n", "<leader>hD", function()
 				gs.diffthis("~")
-			end, "diff ~")
+			end, "gitsigns: diff ~")
 		end,
 	},
 }
