220ca3df92c3df50208162db4ef6c9935ec1901d
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2025-04-05 11:55:03 +0200

    refactor(copilot): updated copilot.nvim configuration

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,6 @@
+-- copilot.lua
+-- https://github.com/zbirenbaum/copilot.lua
+
 return {
 	"zbirenbaum/copilot-cmp",
 	dependencies = {
@@ -11,9 +14,36 @@ return {
 			filetypes = {
 				markdown = true,
 				gitcommit = true,
+				terraform = false,
+				sh = function()
+					-- disable for .env files
+					if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
+						return false
+					end
+					return true
+				end,
+			},
+			copilot_model = "Claude 3.7 Sonnet",
+			server_opts_overrides = {
+				trace = "verbose",
+				settings = {
+					listCount = 10,
+					inlineSuggestCount = 3,
+					timeoutMs = 500,
+					debounce = 250,
+				},
 			},
 		})
 
-		require("copilot_cmp").setup()
+		require("copilot_cmp").setup({
+			method = "getCompletionsCycling",
+			formatters = {
+				label = require("copilot_cmp.format").format_label_text,
+				insert_text = require("copilot_cmp.format").format_insert_text,
+				preview = require("copilot_cmp.format").deindent,
+			},
+			event = { "InsertEnter", "LspAttach" },
+			fix_pairs = true,
+		})
 	end,
 }
